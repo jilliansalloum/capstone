b5193773e75479d479e7472fe4e8ae33920f67ec
Jillian Salloum <salloum1@msu.edu>
2022-08-29 17:59:09 -0400

    !

--- public/app-index.js
+++ public/app-index.js
@@ -2,8 +2,6 @@ const mainContainer = document.querySelector('.container')
 
 const baseURL = `http://localhost:3000`
 
-const menu1 = document.querySelector('#mobile-menu')
-const menu1Links = document.querySelector('.navbar__menu')
 
 const itemsCallback = ({data : posts}) => displayItems(posts)
 const errCallback = err => console.log(err)
@@ -15,8 +13,8 @@ getPosts()
 
 
 
-menu1.addEventListener('click', function() {
-    menu1.classList.toggle('is-active');
-    menu1Links.classList.toggle('active');
-})
+// menu1.addEventListener('click', function() {
+//     menu1.classList.toggle('is-active');
+//     menu1Links.classList.toggle('active');
+// })
 
